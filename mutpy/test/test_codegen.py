dff78b54ce49c6183b7e78215193bc28ed68b279
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2025-12-07 13:42:03 +0800

    add test for add line numbers

--- mutpy/test/test_codegen.py
+++ mutpy/test/test_codegen.py
@@ -6,13 +6,29 @@ Hello, world!
 
 This is a test.
 
-    Spaces before text
-"""
+    Spaces before text"""
     
     result = codegen.remove_extra_lines(source)
 
     expected = """Hello, world!
 This is a test.
     Spaces before text"""
+
+    assert result == expected
+
+def test_add_line_numbers():
+    source = """Hello, world!
+
+This is a test.
+
+    Spaces before text"""
+    
+    result = codegen.add_line_numbers(source)
+
+    expected = """ 1: Hello, world!
+ 2: 
+ 3: This is a test.
+ 4: 
+ 5:     Spaces before text"""
 
     assert result == expected
