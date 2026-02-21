a10f31451a0dc218c6996fabcd6d1f975823b862
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-02-22 01:15:32 +0800

    chore: clean up codegen module

--- mutpy/test/test_codegen.py
+++ mutpy/test/test_codegen.py
@@ -1,28 +1,15 @@
+"""Unit tests for codegen.py."""
+
 from mutpy import codegen
 
-def test_remove_extra_lines():
-    source = """
-Hello, world!
 
-This is a test.
-
-    Spaces before text"""
-    
-    result = codegen.remove_extra_lines(source)
-
-    expected = """Hello, world!
-This is a test.
-    Spaces before text"""
-
-    assert result == expected
-
-def test_add_line_numbers():
+def test_add_line_numbers() -> None:
     source = """Hello, world!
 
 This is a test.
 
     Spaces before text"""
-    
+
     result = codegen.add_line_numbers(source)
 
     expected = """ 1: Hello, world!
@@ -30,5 +17,22 @@ This is a test.
  3: This is a test.
  4: 
  5:     Spaces before text"""
+
+    assert result == expected
+
+
+def test_remove_extra_lines() -> None:
+    source = """
+Hello, world!
+
+This is a test.
+
+    Spaces before text"""
+
+    result = codegen.remove_extra_lines(source)
+
+    expected = """Hello, world!
+This is a test.
+    Spaces before text"""
 
     assert result == expected
